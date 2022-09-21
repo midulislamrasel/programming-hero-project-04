aec71184db1987737fdfba2f8504ac82a4f727f3
Midul-islam-Russell <midulraddy121@gmail.com>
2022-09-21 16:51:11 +0600

    third Problem

--- Assignment04.js
+++ Assignment04.js
@@ -46,3 +46,26 @@ function isJavaScriptFile(jsFile) {
     }
 }
 console.log(isJavaScriptFile('zabu.js'));
+
+
+
+//---- 0.3: Calculate the total oil price that I have pay.
+
+function oilPrice(diesel,
+    petrol, octane) {
+    const perDiesel = 114;
+    const perPetrol = 130;
+    const perOctan = 135;
+
+    const diselOil = diesel * perDiesel;
+    const petrolOil = petrol * perPetrol;
+    const octanOil = octane * perOctan;
+
+    const totalOil = diselOil + petrolOil + octanOil;
+
+    return totalOil;
+}
+
+
+const totalOil = oilPrice(5, 3, 6);
+console.log(totalOil);
