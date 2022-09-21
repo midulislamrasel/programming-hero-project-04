af74eb3adebc34cf297fa15f3e45e9614850c6ed
Midul-islam-Russell <midulraddy121@gmail.com>
2022-09-21 16:48:09 +0600

    second problem

--- Assignment04.js
+++ Assignment04.js
@@ -15,3 +15,34 @@ function radianToDegree(radians) {
 }
 
 console.log(radianToDegree(230));
+
+
+
+
+//---- 0.2: Check whether the given file name is a javascript file or not--
+
+function isJavaScriptFile(jsFile) {
+    let chekUnDefined = typeof jsFile;
+    if (chekUnDefined === "undefined") {
+        return "Please Enter a Valid Input";
+    } else {
+        const fileName1 = 'zabu.js';
+        const fileName2 = 'jarbu.pdf';
+        const fileName3 = 'jabur.png';
+
+        if (jsFile === fileName1) {
+            return true;
+
+        }
+        else if (jsFile === fileName2) {
+            return false;
+        }
+        else if (jsFile === fileName3) {
+            return false;
+        }
+        else {
+            return false;
+        }
+    }
+}
+console.log(isJavaScriptFile('zabu.js'));
